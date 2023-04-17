321a2d0a3c2480e88674e36b07d48f348e45f23c
Tejas <iamtejasbhatt02@gmail.com>
2023-04-17 13:50:35 +0530

    Page Slide Animation

---
+++ lib/initial_pages/page_first.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:my_page_view/initial_pages/page_second.dart';
+
+class PageFirst extends StatelessWidget {
+  const PageFirst({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('jump animation'),
+      ),
+      body: Column(
+        children: [
+          TextButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    PageRouteBuilder(
+                      pageBuilder: (BuildContext context,
+                          Animation<double> animation,
+                          Animation<double> secondaryAnimation) {
+                        return SecondPage();
+                      },
+                      transitionsBuilder: (BuildContext context,
+                          Animation<double> animation,
+                          Animation<double> secondaryAnimation,
+                          Widget child) {
+                        return SlideTransition(
+                          position: Tween<Offset>(
+                            begin: const Offset(1.0,
+                                0.0), // starting position of the new screen
+                            end: Offset
+                                .zero, // ending position of the new screen
+                          ).animate(animation),
+                          child: child,
+                        );
+                      },
+                    ));
+              },
+              child: Text('Go to Second Page'))
+        ],
+      ),
+    );
+  }
+}
